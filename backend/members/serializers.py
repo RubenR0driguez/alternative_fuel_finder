7cd5057b02b22b8d23059a29ec4602126e166454
ruben rodriguez <rotap45r@gmail.com>
2022-05-27 17:52:36 -0400

    modified the the url endpoints on the members app and tested on postman new locations and review comments still need to be modified in order to complete all endpoint and views.py files

--- backend/members/serializers.py
+++ backend/members/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Members
 
 
-class membersserializer(serializers.ModelSerializer):
+class Membersserializer(serializers.ModelSerializer):
     class Meta:
         model = Members
         fields = ['id', 'first_name', 'last_name', 'email_address']
--- backend/members/views.py
+++ backend/members/views.py
@@ -1,9 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework import status
+from .models import Members
+from .serializers import Membersserializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def members_list(request):
 
-    return Response('ok')
+    if request.method == 'GET':
+        members = Members.objects.all()
+        serializer = Membersserializer(members, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = Membersserializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
